260f111ca1eda743dc5bf7caf0ad0184c7503992
Frazenilio <121224911+Frazenilio@users.noreply.github.com>
2025-07-05 18:47:04 +0200

    New: GrigliaMatrix metodi
    Aggiunti i metodi mancanti di GrigliaMatrix per aggiungere un ostacolo (usando l'or tra bit) e il from per creare una GrigliaMatrix (usando Cella)

--- src/francesco/GrigliaMatrix.java
+++ src/francesco/GrigliaMatrix.java
@@ -1,7 +1,8 @@
 package francesco;
 
 import java.util.List;
-import java.util.function.BiConsumer;
+
+import francesco.implementazioni.Cella;
 
 public record GrigliaMatrix(ICella[][] mat) implements IGriglia<ICella> {
 	
@@ -27,18 +28,30 @@ public record GrigliaMatrix(ICella[][] mat) implements IGriglia<ICella> {
 	
 	@Override
 	public IGriglia<ICella> addObstacle(IObstacle obstacle) {
-		// TODO
-		ICella[][] mat = new ICella[height()][width()];
+//		ICella[][] mat = new ICella[height()][width()];
 		obstacle.list().forEach(c -> {
-			//mat[c.y()][c.x()] |= ostacolo;
+			mat()[c.y()][c.x()].setStato(mat()[c.y()][c.x()].stato() | c.stato());
 		});
-		//return new GrigliaMatrix(mat);
-		throw new UnsupportedOperationException();
+		return new GrigliaMatrix(mat);
 	}
 	
 	
 	public static IGriglia<ICella> from(int width, int height, List<IObstacle> ostacoli) {
-		throw new UnsupportedOperationException();
+		ICella[][] mat = inizializzaMatrice(width, height);
+		IGriglia<ICella> griglia = new GrigliaMatrix(mat);
+		for(IObstacle o : ostacoli) {
+			griglia = griglia.addObstacle(o);
+		}
+		return griglia;
 	}
 	
+	public static Cella[][] inizializzaMatrice(int width, int height) {
+		Cella[][] mat = new Cella[height][width];
+		for(int i = 0; i < height; i++) {
+			for(int j = 0; j < width; j++) {
+				mat[i][j] = new Cella(StatoCella.VUOTA.value()); 
+			}
+		}
+		return mat;
+	}
 }
